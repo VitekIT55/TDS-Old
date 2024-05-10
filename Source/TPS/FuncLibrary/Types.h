faecb0096426f0aaceffcae9373427374dcdff3a
VitekIT55 <trakmanic80@gmail.com>
2024-05-10 13:29:24 +0300

    fix 1.4

---
+++ Source/TPS/FuncLibrary/Types.h
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "Types.generated.h"
+
+UENUM(BlueprintType)
+enum class EMovementState : uint8
+{
+	Aim_State UMETA(DisplayName = "Aim State"),
+	AimWalk_State UMETA(DisplayName = "AimWalk State"),
+	Walk_State UMETA(DisplayName = "Walk State"),
+	Run_State UMETA(DisplayName = "Run State"),
+	Sprint_State UMETA(DisplayName = "Sprint State"),
+};
+
+USTRUCT(BlueprintType)
+struct FChatacterSpeed
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement")
+	float AimSpeedNormal = 300.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement")
+	float WalkSpeedNormal = 200.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement")
+	float RunSpeedNormal = 600.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement")
+	float AimSpeedWalk = 100.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement")
+	float RunSpeedSprint = 800.0f;
+};
+
+UCLASS()
+class TPS_API UTypes : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+};
